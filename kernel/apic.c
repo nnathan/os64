4e4ada3843fcccea1625021885f4c366202fc1e3
Charles E. Youse <charles@gnuless.org>
2019-10-15 10:28:36 -0400

    kernel: apic.c: add IPI support
    Add support for sending IPIs via the local APIC's ICR, and a
    not-very-robust function to send the init/startup sequence
    required to start APs.
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- kernel/apic.c
+++ kernel/apic.c
@@ -36,21 +36,26 @@
 #define LAPIC_WRITE(r,v) (*((unsigned *) (LAPIC_BASE + ((r) << 4))) = (v))
 
 #define LAPIC_ID            0x02        /* identification */
+#define LAPIC_EOI           0x0B        /* end of interrupt */
+#define LAPIC_SPUR          0x0F        /* spurious interrupt vector */
 #define LAPIC_TMRx          0x18        /* trigger modes (TMR0..7 0x18-0x1F) */
-
 #define LAPIC_CMCI_LVT      0x2F        /* corrected machine check */
+#define LAPIC_ICRLO         0x30        /* interrupt command[31:0] */
+#define LAPIC_ICRHI         0x31        /* interrupt command[63:32] */
 #define LAPIC_TIMER_LVT     0x32        /* timer */
 #define LAPIC_THERMAL_LVT   0x33        /* thermal monitor */
 #define LAPIC_PERF_LVT      0x34        /* performance counter */
-#define LAPIC_LINT0_LVT     0x35        /* interrupt pin 0 */
-#define LAPIC_LINT1_LVT     0x36        /* interrupt pin 1 */
+#define LAPIC_LINT0_LVT     0x35        /* local interrupt pin 0 */
+#define LAPIC_LINT1_LVT     0x36        /* local interrupt pin 1 */
 #define LAPIC_ERROR_LVT     0x37        /* internal error */
+
 #define LAPIC_LVT_MASK      0x00010000  /* common to *_LVT: LVT is masked */
-
-#define LAPIC_EOI           0x0B        /* end of interrupt */
-
-#define LAPIC_SPUR          0x0F        /* spurious interrupt vector */
 #define LAPIC_SPUR_ENABLE   0x00000100  /* APIC enable bit */
+#define LAPIC_ICRLO_BUSY    0x00001000  /* IPI delivery in progress */
+
+#define LAPIC_ICR_IPI_OTHERS    0x000C4000  /* regular IPI to other CPUs */
+#define LAPIC_ICR_IPI_INIT      0x00004500  /* init IPI to target CPU */
+#define LAPIC_ICR_IPI_STARTUP   0x00004600  /* startup IPI to target CPU */
 
 /* called by each CPU during startup. this is probably overly pedantic, as
    the firmware should leave the local APICs in a well-defined state. also,
@@ -70,6 +75,40 @@ lapic_init()
     LAPIC_WRITE(LAPIC_ERROR_LVT, LAPIC_LVT_MASK);
 
     LAPIC_WRITE(LAPIC_EOI, 0);      /* clear any pending interrupt */
+}
+
+/* internal use only, for lapic_startcpu() and lapic_schedipi() */
+
+static
+lapic_ipi(target, ipi, vector)
+{
+    long flags;
+
+    ipi |= vector;
+
+    flags = lock();
+
+    while (LAPIC_READ(LAPIC_ICRLO) & LAPIC_ICRLO_BUSY)
+        /* wait until not busy */ ;
+
+    LAPIC_WRITE(LAPIC_ICRHI, target << 24);
+    LAPIC_WRITE(LAPIC_ICRLO, ipi);
+
+    unlock(flags);
+}
+
+/* fire up another CPU (identified by its APIC ID). this is not robust and
+   will just hang if the CPU never comes up. also, the delay loop is bogus;
+   once we have some kind of driver for a timer source we should use that. */
+
+lapic_startcpu(target, entry)
+int entry();
+{
+    int i;
+
+    lapic_ipi(target, LAPIC_ICR_IPI_INIT, 0);
+    for (i = 0; i < 1000000; ++i) ;
+    lapic_ipi(target, LAPIC_ICR_IPI_STARTUP, ADDR_TO_PGNO(entry));
 }
 
 /* I/O APIC definitions. like the CMOS or CRTC, the I/O APIC only directly
